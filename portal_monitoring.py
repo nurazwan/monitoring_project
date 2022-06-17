53f1e83936393e202d3be6e55c740c274500f15d
Nurazwan Hasan <nurazwanhasan@icloud.com>
2022-06-17 14:06:37 +0800

    initial commit

---
+++ portal_monitoring.py
@@ -0,0 +1,31 @@
+import datetime as dt
+import numpy as np
+import pandas as pd
+import requests as r
+import sys
+import time
+
+while True:
+    #connect to db
+    
+    portal=pd.read_csv('portals.csv')
+    portal=portal[portal.Mute==False]
+    timestamp=str((dt.datetime.today()))
+    for i in range(len(portal)):
+        try:
+            if r.get(portal.uri[i],timeout=3).status_code==200:
+                status=1
+            else:
+                status=0
+            time_elapsed=r.get(portal.uri[i],timeout=3).elapsed.total_seconds()
+            
+        except:
+            status=0
+            time_elapsed=100
+            
+        
+        # write to db
+        #close connection to db
+        print([timestamp,portal.uri[i],status,time_elapsed])
+    
+    time.sleep(5)
